71964a2f9e9f202c001ccf06ada2afd30b409c83
r-tkbys <takeryo.maniac.sound@gmail.com>
2026-06-29 23:10:56 +0900

    Bump version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.takemotions.mediabridge"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.1.7"
+        versionCode = 9
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
